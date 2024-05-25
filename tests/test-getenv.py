1f5465f2ce65f83235cf17622aba524774fcb3fc
Thomas Hamilton <88941733+tlh45342@users.noreply.github.com>
2024-05-25 11:10:53 -0400

    Create test-getenv.py

---
+++ tests/test-getenv.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+
+def main():
+    load_dotenv()  # take environment variables from .env.
+
+    print("Username:", os.getenv('USERNAME'))
+    print("Password:", os.getenv('PASSWORD'))
+    print("Servername:", os.getenv('SERVERNAME'))
+
+# --------------------    
+
+main()
